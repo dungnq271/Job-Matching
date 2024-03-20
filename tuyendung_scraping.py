a6ba91a387f93a947dab5574b05c7d5e0b6c0246
dungnq271 <dungnguyen2712002@gmail.com>
2024-03-20 18:53:54 +0700

    Update scraping code

--- tuyendung_scraping.py
+++ tuyendung_scraping.py
@@ -1,16 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
+
 from selectolax.parser import HTMLParser
-
-import itertools
-from pprint import pprint
+import time
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
-
 
 options = Options()
 options.add_experimental_option("detach", True)  # keep the window open
@@ -24,31 +22,43 @@ driver.get(url)
 driver.maximize_window()
 
 
-df = {
-    "role": [],
-    "company": [],
-    "location": [],
-    "link": [],
-    # "posted": [],
-    # "salary": [],
-    # "type": [],
-    "metadata": [],
-    "description": []
+xpaths = {
+    'Logo'            :"./div[1]//img",
+    'Role'            :"./div[2]",
+    'Company'         :"./div[4]/div/div[1]",
+    'Location'        :"./div[4]/div/div[2]",
+    'Source'          :"./div[4]/div/div[3]",
+    'Posted'          :".//*[name()='path'][contains(@d,'M11.99')]/ancestor::div[1]",
+    'Full / Part Time':".//*[name()='path'][contains(@d,'M20 6')]/ancestor::div[1]",
+    'Salary'          :".//*[name()='path'][@fill-rule='evenodd']/ancestor::div[1]",
+    'Description'     :"//div[contains(@class, 'pE8vnd')][.//div[contains(@class, 'YgLbBe')]][.//span[@class='HBvzbc']]"
 }
-cols = list(df.keys())
+
+data = {key:[] for key in xpaths}
+jobs_to_do = 20
+jobs_done = 0
+
+while jobs_done < jobs_to_do:
+    lis = driver.find_elements("xpath", "//li[@data-ved]//div[@role='treeitem']/div/div")
+    
+    for li in lis[jobs_done:]:
+        li.click()
+        driver.execute_script('arguments[0].scrollIntoView({block: "center", behavior: "smooth"});', li)
+
+        for key in xpaths:
+            try:
+                if key == "Description":
+                    t = driver.find_elements("xpath", xpaths[key])[-1].text
+                else:
+                    t = li.find_element("xpath", xpaths[key]).get_attribute('src' if key=='Logo' else 'innerText')
+            except NoSuchElementException:
+                t = '*missing data*'
+            data[key].append(t)
+        
+        jobs_done += 1
+        print(f'{jobs_done=}', end='\r')
+        time.sleep(.2)
 
 
-text = driver.page_source
-html = HTMLParser(text)
-
-job_block = html.css_first("div[class~='pE8vnd']")
-
-print(job_block.css_first("h2.SBkjJd").text(deep=True, separator=' ', strip=False))
-
-
-buttons = driver.find_elements(
-    "xpath",
-    "//div[@role='button' and @class='Fol1qc']"
-)
-num_jobs = len(buttons)
-print(num_jobs)
+df = pd.DataFrame(data)
+df.to_csv("job_vn_posted.csv", index=False)        
